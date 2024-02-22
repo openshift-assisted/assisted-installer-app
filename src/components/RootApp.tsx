11ce5d929f8384b20f2c091c400f7af7a9a27cdf
Guillaume Vincent <gvincent@redhat.com>
2024-02-22 11:25:46 +0100

    Configure base url for openshift API

--- src/components/RootApp.tsx
+++ src/components/RootApp.tsx
@@ -1,8 +1,23 @@
-import React, { useEffect } from 'react';
-import { Config, Routes } from '@openshift-assisted/ui-lib/ocm';
+import React, { useEffect, useState } from 'react';
+import { Api, Config, Routes } from '@openshift-assisted/ui-lib/ocm';
 import type { FeatureListType } from '@openshift-assisted/ui-lib/common';
 import { BrowserRouter } from 'react-router-dom';
+import { AxiosInstance, InternalAxiosRequestConfig } from 'axios';
 import '../i18n';
+
+const apiGateway = 'https://api.stage.openshift.com';
+
+const authInterceptor = (client: AxiosInstance): AxiosInstance => {
+  client.interceptors.request.use((config) => {
+    const BASE_URL = config.baseURL || apiGateway;
+    const updatedConfig: InternalAxiosRequestConfig = {
+      ...config,
+      url: `${BASE_URL}${config.url}`,
+    };
+    return updatedConfig;
+  });
+  return client;
+};
 
 function RootApp({
   allEnabledFeatures,
@@ -11,9 +26,15 @@ function RootApp({
   allEnabledFeatures: FeatureListType;
   routeBasePath?: string;
 }) {
+  const [hasNotBeenSetted, setHasNotBeenSetted] = useState(true);
+
   useEffect(() => {
     Config.setRouteBasePath(routeBasePath);
-  }, []);
+    if (hasNotBeenSetted) {
+      Api.setAuthInterceptor(authInterceptor);
+      setHasNotBeenSetted(false);
+    }
+  }, [hasNotBeenSetted]);
 
   return (
     <React.StrictMode>
