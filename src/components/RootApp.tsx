6ab9cc2b7940c6f453de83a2bf804c4044dc0191
Guillaume Vincent <gvincent@redhat.com>
2024-02-22 11:50:45 +0100

    Use AxiosRequestConfig instead of InternalAxiosRequestConfig

--- src/components/RootApp.tsx
+++ src/components/RootApp.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Api, Config, Routes } from '@openshift-assisted/ui-lib/ocm';
 import type { FeatureListType } from '@openshift-assisted/ui-lib/common';
 import { BrowserRouter } from 'react-router-dom';
-import { AxiosInstance, InternalAxiosRequestConfig } from 'axios';
+import { AxiosInstance, AxiosRequestConfig } from 'axios';
 import '../i18n';
 
 const apiGateway = 'https://api.stage.openshift.com';
@@ -10,7 +10,7 @@ const apiGateway = 'https://api.stage.openshift.com';
 const authInterceptor = (client: AxiosInstance): AxiosInstance => {
   client.interceptors.request.use((config) => {
     const BASE_URL = config.baseURL || apiGateway;
-    const updatedConfig: InternalAxiosRequestConfig = {
+    const updatedConfig: AxiosRequestConfig = {
       ...config,
       url: `${BASE_URL}${config.url}`,
     };
