ed96de6f8b2627fa6aa042b01797f74348e02f4e
Montse Ortega <mortegag@redhat.com>
2024-08-08 12:59:52 +0200

    Remove feature flags

--- src/components/RootApp.tsx
+++ src/components/RootApp.tsx
@@ -1,9 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import { Api, Config, Routes } from '@openshift-assisted/ui-lib/ocm';
+import {
+  Api,
+  Config,
+  UILibRoutes as Routes,
+} from '@openshift-assisted/ui-lib/ocm';
 import { BrowserRouter } from 'react-router-dom';
 import { AxiosInstance, AxiosRequestConfig } from 'axios';
 import '../i18n';
 import useChrome from '@redhat-cloud-services/frontend-components/useChrome';
+import { CompatRouter } from 'react-router-dom-v5-compat';
 
 const apiGatewayStage = 'https://api.stage.openshift.com';
 const apiGatewayProd = 'https://api.openshift.com';
@@ -33,7 +38,7 @@ export const buildAuthInterceptor = (
 };
 
 function RootApp({
-  routeBasePath = '/assisted-installer',
+  routeBasePath = '/assisted-installer-app',
 }: {
   routeBasePath?: string;
 }) {
@@ -50,7 +55,9 @@ function RootApp({
   return (
     <React.StrictMode>
       <BrowserRouter basename={'/openshift'}>
-        <Routes />
+        <CompatRouter>
+          <Routes allEnabledFeatures={{}} />
+        </CompatRouter>
       </BrowserRouter>
     </React.StrictMode>
   );
