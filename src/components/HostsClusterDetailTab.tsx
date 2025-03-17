8bf308e141736f436095012adc85db927e962883
Montse Ortega <mortegag@redhat.com>
2025-03-17 07:21:40 +0100

    Adding HostsClusterDetailTab component

--- src/components/HostsClusterDetailTab.tsx
+++ src/components/HostsClusterDetailTab.tsx
@@ -1,3 +1,12 @@
-import { HostsClusterDetailTab } from '@openshift-assisted/ui-lib/ocm';
+import * as React from 'react';
+import { HostsClusterDetailTab as AIHostsClusterDetailTab } from '@openshift-assisted/ui-lib/ocm';
+import { useInitApp } from '../hooks/useInitApp';
 import '../i18n';
+import { HostsClusterDetailTabProps } from '@openshift-assisted/ui-lib/build/@types/ocm/components/HostsClusterDetailTab/types';
+
+const HostsClusterDetailTab: React.FC<HostsClusterDetailTabProps> = (props) => {
+  useInitApp();
+  return <AIHostsClusterDetailTab {...props} />;
+};
+
 export default HostsClusterDetailTab;
