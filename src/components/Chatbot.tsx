152d96160def2519c235660604992790d604ebdf
rawagner <rawagner@redhat.com>
2025-08-21 18:19:46 +0200

    Fix chatbot URL

--- src/components/Chatbot.tsx
+++ src/components/Chatbot.tsx
@@ -8,6 +8,7 @@ import { useNavigate } from 'react-router-dom-v5-compat';
 
 import { useAMSCapability } from '../hooks/useAMSCapability';
 import { useUsername } from '../hooks/useUsername';
+import { getBaseUrl } from '../hooks/useInitApp';
 
 import '@patternfly-6/chatbot/dist/css/main.css';
 import './Chatbot.scss';
@@ -23,7 +24,7 @@ const ChatBot = () => {
   const onApiCall = React.useCallback<ChatBotWindowProps['onApiCall']>(
     async (input, init) => {
       const userToken = await chrome.auth.getToken();
-      const api = new URL(window.ocmConfig?.configData?.apiGateway || '');
+      const api = new URL(getBaseUrl());
       return fetch(`https://assisted-chat.${api.hostname}${input}`, {
         ...(init || {}),
         headers: {
