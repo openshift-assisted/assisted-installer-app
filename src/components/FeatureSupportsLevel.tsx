94218fc82635060aaa48d77e6e83aeac3bac6967
jgyselov <jgyselov@redhat.com>
2026-08-11 11:40:45 +0200

    Fix feature support level exports

--- src/components/FeatureSupportsLevel.tsx
+++ src/components/FeatureSupportsLevel.tsx
@@ -1,3 +1,3 @@
-import { FeaturesSupportsLevel } from '@openshift-assisted/ui-lib/ocm';
+import { FeaturesSupportsLevel } from '@openshift-assisted/ui-lib/common';
 import '../i18n';
 export type { FeaturesSupportsLevel as default };
